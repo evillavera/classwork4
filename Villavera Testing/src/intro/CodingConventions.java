a0878586da62fafe2a02c24c508430b90f090e17
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-13 11:00:29 -0400

    Coding Conventions Completed (Nockles File)

--- Villavera Testing/src/intro/CodingConventions.java
+++ Villavera Testing/src/intro/CodingConventions.java
@@ -1,26 +1,36 @@
 package intro;
 
-//class is a file describing a data type
+//class is a file describing a new data type 
 public class CodingConventions {
 
 	
-	//fields(has-a relationship)
+	//fields
 	private String name;
 	private String description;
 	
-	
 	/*
-	this is a special method, a "constructor"
-	notice it does not have a return type, 
-	that is because the thing it returns is an instance of the class
-	therefore the name of a constructor must always match
-	the name of the class
-	the fields made in the class is put into the constructor
-	we assign value to the fields inside the constructor
-	*/
-	public CodingConventions() {
-		//in Java, fields are instantiated in a constructor
-		name = "Villavera";
-		description = "Student";
+	//this is a special method, a "constructor"
+	//notice it does not have a return type,
+	//that is because the thing it returns is an instance of the class
+	 * therefore the name of a constructor must always
+	 *  match the name of the class
+	 */
+	public CodingConventions(String name, int index){
+		//In Java, fields are instantiated in a constructor
+		//This make it into a field which means it is being referenced.
+		//There is a local variable called 'name' and a field called 'name'
+		//The field persists, but the local variable data is destroyed at the end of this method
+		//Distinguish the field by using the reserved word "this"
+		
+		this.name = name;
+		//static reference begins with a class name
+		description = IntroMain.DESCRIPTIONS[index];
 	}
+
+	public void doStuff() {
+		//static method call (does not change)
+		System.out.println(name + description);//use a space between your operations
+		System.out.print("The square root of 12 is "+ Math.sqrt(12));
+	}
+	
 }
--- Villavera Testing/src/intro/IntroMain.java
+++ Villavera Testing/src/intro/IntroMain.java
@@ -1,20 +1,24 @@
-package intro;//new changes made 9/12/17 finish work Erik Villavera
+package intro;//this is a package declaration
 
 //this is a class header (class declaration)
 public class IntroMain {
 
-	
+	//constants (public)
+	public static final String[] DESCRIPTIONS = {" is a teacher", 
+			" is a student",
+			" is a human being",
+			" is imaginary",};
+	                                             
 	
 	public static void main(String[] args) {
 		//declaration of a local variable
-		//notice the use of the word new
+		//notice the use of the word 'new'
 		//new must be used to call a constructor
-		//CodingConventions is the name of the class
-		//conventionsInstance is the variablename/handle
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Erik",1);
 		
 		//instance method call
 		conventionsInstance.doStuff();
 	}
 
 }
+
