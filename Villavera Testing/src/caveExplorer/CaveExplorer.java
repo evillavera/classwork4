54b68caa0e45c45dae8fada17ae6f9f72cc75884
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-10-27 11:46:17 -0400

    fasdfa

--- Villavera Testing/src/caveExplorer/CaveExplorer.java
+++ Villavera Testing/src/caveExplorer/CaveExplorer.java
@@ -8,10 +8,28 @@ public class CaveExplorer {
 	public static Scanner in;//for user input
 	public static CaveRoom currentRoom;//changes based on how the user navigated 
 	public static Inventory inventory;// where all objects found in cave are kept
+	public static boolean playing = true;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		in = new Scanner(System.in);
+		CaveRoom.setUpCaves();
+		
+		inventory = new Inventory();
+		startExploring();
 	}
 
+	private static void startExploring() {
+		while(playing) {
+			print(inventory.getDescription());
+			print(currentRoom.getDescription());
+			print("What would you like to do?");
+			String input = in.nextLine();
+			currentRoom.interpretInput(input);
+		}
+	}
+
+	public static void print(String s) {
+		//NOTE: later, you can replace this line with the more sophisticated "multiLinePrint" function from ChatBot
+		System.out.println(s);
+	}
 }
--- Villavera Testing/src/caveExplorer/CaveRoom.java
+++ Villavera Testing/src/caveExplorer/CaveRoom.java
@@ -102,7 +102,7 @@ public class CaveRoom {
 	}
 	
 	public void interpretInput(String input) {
-		while(isValid(input)) {
+		while(!isValid(input)) {
 			System.out.println("You can only enter 'w', 'a', 's' or 'd'.");
 			input = CaveExplorer.in.nextLine();
 		}
@@ -135,11 +135,32 @@ public class CaveRoom {
 	 * and all the connections
 	 */
 	public static void setUpCaves() {
+		//ALL OF THIS CODE CAN BE CHANGED
+		//1. Decide how big your cave should be
+		CaveExplorer.caves = new CaveRoom[5][5];
+		//2. Populate with caves and a default description. Hint: when starting, use coordinates. (helps debugging)
+		//LENGTH
+		for(int row = 0; row < CaveExplorer.caves.length; row++) {
+			//PLEASE PAY ATTENTION TO THE DIFFERENCE
+			//WIDTH
+			for(int col = 0; col < CaveExplorer.caves[0].length; col++) {
+				//create a "default" cave
+				CaveExplorer.caves[row][col] = new CaveRoom("This cave has coords ("+row+","+col+")");
+			}
+		}
+		//3. Replace default rooms with custom rooms
+		//--- WE WILL DO LATER
 		
+		//4. Set your starting room:
+		CaveExplorer.currentRoom = CaveExplorer.caves[0][1];
+		CaveExplorer.currentRoom.enter();
+		//5. Set up doors
+		CaveRoom[][] c = CaveExplorer.caves;
+		c[0][1].setConnection(SOUTH, c[1][1], new Door());
 	}
 	
 	public String getDescription() {
-		return description;
+		return description + "\n" + directions;
 	}
 
 	public void setDescription(String description) {
