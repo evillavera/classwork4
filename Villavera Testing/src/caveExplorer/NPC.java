87a7c5e154e3d023c084ea52963c21d599cda642
BT_1N3_19 <xchen7640@bths.edu>
2017-10-31 11:30:50 -0400

    asdf

---
+++ Villavera Testing/src/caveExplorer/NPC.java
@@ -0,0 +1,30 @@
+package caveExplorer;
+
+/**
+ * NPC means Non-Playable Character
+ * @author BT_1N3_19
+ *
+ */
+
+public class NPC {
+
+	public NPC() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void interact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getInactiveDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
