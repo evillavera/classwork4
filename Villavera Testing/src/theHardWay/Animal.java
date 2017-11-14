422be6f2297d3f2326e2d2222651d04bb5b1b3bb
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-14 10:46:16 -0500

    theHardWay

---
+++ Villavera Testing/src/theHardWay/Animal.java
@@ -0,0 +1,49 @@
+package theHardWay;
+
+public class Animal {
+
+	private String description;
+	
+	private Trait trait1;
+	private Trait trait2;
+	private int age;
+	private boolean mated;
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			Animal someAnimal
+		}
+
+	}
+	
+	public Animal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isMated() {
+		return mated;
+	}
+
+	public void setMated(boolean mated) {
+		this.mated = mated;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Trait getTrait1() {
+		return trait1;
+	}
+
+	public Trait getTrait2() {
+		return trait2;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+
+}
