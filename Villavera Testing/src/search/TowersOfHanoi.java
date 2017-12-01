e19b98aecc2fb2fbb694ce0f2c2704aa3283a466
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-12-01 11:47:55 -0500

    fasdf

---
+++ Villavera Testing/src/search/TowersOfHanoi.java
@@ -0,0 +1,19 @@
+package search;
+
+public class TowersOfHanoi {
+
+	/*
+	 * 3 disk = 7 moves
+	 * 4 disk = 15 moves
+	 * 8 moves to move base plate ^^
+	 * 7 moves to move the rest
+	 * 5 disk = 
+	 * 16 to move base plate
+	 */
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
