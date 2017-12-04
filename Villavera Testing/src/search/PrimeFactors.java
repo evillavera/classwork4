73a87f43a3fa43dcebdbdeebc1b8c570b610f1e5
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-12-04 11:29:30 -0500

    fasdf

--- Villavera Testing/src/search/PrimeFactors.java
+++ Villavera Testing/src/search/PrimeFactors.java
@@ -4,11 +4,17 @@ public class PrimeFactors {
 	private static int number = 0;
 	
 	 public static void main(String[] args){
-		 int value = 18;
+		 int value = 30;
+		 int value2 = 18;
+		 String word = "hello";
+		 /*
 		 System.out.println(value+"! is equal to "+factorial(value));
 		 System.out.println(value+ " has "+countPrimeFactors(value, 2)+" prime factors.");
+		 */
+		 System.out.println(word+"! is equal to "+recursiveMethod(word));
+		// System.out.println(mystery(value,value2));
 		 }
-
+/*
 		 public static int countPrimeFactors(int value, int testPrime) {
 		 if(value == 1) {
 			 System.out.println("The number " + testPrime + " has been tested "
@@ -22,7 +28,7 @@ public class PrimeFactors {
 			return countPrimeFactors(value/testPrime,testPrime);
 		 }
 		 }
-
+*/
 		 public static int factorial(int value) {
 		 if(value == 1) {
 			 return 1;
@@ -31,6 +37,16 @@ public class PrimeFactors {
 		 }
 		 }
 
+		 
+		 public static String recursiveMethod(String input) {
+			 if(input.length() == 1) {
+				 return input;
+			 }else {
+				 return input.substring(input.length() - 1, input.length()) + 
+						 recursiveMethod(input.substring(0, input.length() -1));
+			 }
+		 }
+		 
 		 /**
 		  * Sam's Code
 		  *  public static int countPrimeFactors(int value, int testPrime) {
